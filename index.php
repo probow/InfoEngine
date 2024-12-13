b521bf994ef32e59204e0a2750c961ac37740eee
Mydo <53073740+probow@users.noreply.github.com>
2024-12-13 20:14:46 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome to the Entertainment Hub</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div id="intro">
+        <h1>Welcome to the Entertainment Hub</h1>
+        <p>Select a category:</p>
+        <a href="movies.php"><button class="button">Movies</button></a>
+        <a href="games.php"><button class="button">Games</button></a>
+        <a href="anime.php"><button class="button">Anime</button></a>
+        <a href="about.php"><button class="button">About Me</button></a>
+        <a href="contact.php"><button class="button">Contact</button></a>
+    </div>
+</body>
+</html>
